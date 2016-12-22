a7e7fd868468109961b49bbd3722a39c3921ac3a
Stephen Whitmore <sww@eight45.net>
2016-12-22 15:35:27 -0500

    Send as base64.

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 var button = document.getElementById('button')
+var after = require('after-all')
+var xhr = require('xhr')
 
 var data = {}
 
 button.onclick = function (e) {
   var files = document.getElementById('files').files
-  console.log(files[0])
-  var pending = files.length
+
+  var afterRead = after(doSend)
 
   for (var i = 0; i < files.length; i++) {
     (function (file) {
-      readAsBinaryString(file, function (_, blob) {
-        data[file.name] = blob
-        onDone()
+      var done = afterRead()
+      readAsBase64(file, function (_, blob) {
+        data[file.name] = blob.substring(blob.indexOf(',') + 1)
+        done()
       })
     })(files[i])
   }
 
-  function onDone () {
-    if (--pending === 0) {
-      console.log('done', data)
-    }
+  function doSend (_) {
+    var afterSend = after(onDone)
+
+    Object.keys(data).forEach(function (filename) {
+      var done = afterSend()
+      xhr({
+        body: data[filename],
+        method: 'POST',
+        uri: '/send?filename=' + filename
+      }, function (err, resp, body) {
+        done(err, body)
+      })
+    })
+  }
+
+  function onDone (_) {
+    console.log('all done')
   }
 }
 
-function readAsBinaryString (file, done) {
+function readAsBase64 (file, done) {
   var reader = new window.FileReader()
   reader.addEventListener('load', function (e) {
     done(null, e.target.result)
   })
   reader.addEventListener('error', done)
-  reader.readAsBinaryString(file)
+  reader.readAsDataURL(file)
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var fs = require('fs')
 var http = require('http')
 var url = require('url')
 var querystring = require('querystring')
+var base64 = require('base64-stream')
 
 http.createServer(onRequest).listen(8400)
 
@@ -9,8 +10,8 @@ function onRequest (req, res) {
   // TODO(sww): use ecstatic
   if (req.url === '/index.html' || req.url === '/') {
     fs.createReadStream('./index.html').pipe(res)
-  } else if (req.url === '/index.js') {
-    fs.createReadStream('./index.js').pipe(res)
+  } else if (req.url === '/bundle.js') {
+    fs.createReadStream('./bundle.js').pipe(res)
   } else if (req.method === 'POST' && /^\/send/.test(req.url)) {
     var qs = url.parse(req.url).query
     var kvs = querystring.parse(qs)
@@ -18,7 +19,9 @@ function onRequest (req, res) {
       res.statusCode = 400
       res.end('missing filename query param')
     } else {
-      req.pipe(fs.createWriteStream(kvs.filename))
+      req
+        .pipe(base64.decode())
+        .pipe(fs.createWriteStream(kvs.filename))
       console.log('wrote', kvs.filename, 'to local disk')
     }
   } else {
